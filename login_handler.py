7db5e93158262e9b9963fc4c4ecbfb1798e956c5
Mathew Conder <cybermc.tech@gmail.com>
2023-12-22 15:13:59 -0500

    Refactor login_handler.py for non-headless Firefox execution
    This commit removes the headless execution option from the webdriver.Firefox instantiation in login_handler.py. It enhances readability by renaming the instance variables "username" and "password" to "username_field" and "password_field", respectively. Furthermore, it gets rid of the auto-execution of the login process when the file runs, providing more flexibility and control.
    
    Signed-off-by: Mathew Conder <cybermc.tech@gmail.com>

--- login_handler.py
+++ login_handler.py
@@ -1,30 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-class LoginBackend():
+class LoginBackend:
     def __init__(self):
-        self.options = Options()
-        self.options.headless = True
-        self.driver = webdriver.Firefox(options=self.options)
+        self.driver = webdriver.Firefox()
         self.driver.get("https://compassmobile.dollartree.com/")
-        self.username = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "idUNField")))
-        self.password = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "idPWField")))
+        self.username_field = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "idUNField")))
+        self.password_field = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "idPWField")))
         self.login_button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn")))
 
     def login(self, username, password):
-        self.username.clear()
-        self.username.send_keys(username)
-        self.password.clear()
-        self.password.send_keys(password)
+        self.username_field.clear()
+        self.username_field.send_keys(username)
+        self.password_field.clear()
+        self.password_field.send_keys(password)
         self.login_button.click()
 
 
 if __name__ == "__main__":
-    login = LoginBackend()
-    login.options.headless = True
-    login.options.enable_mobile()
-    login.login()
+    pass
